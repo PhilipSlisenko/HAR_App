c3178181ca7135e58691e7a8d3d1ec7318e114b4
Philip Slisenko <slisenkophilip@gmail.com>
2019-05-28 17:51:58 +0200

    dlya kosti

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
-
+  constructor(private deviceMotion: DeviceMotion) {}
+  ngOnInit(){
+    this.deviceMotion.getCurrentAcceleration().then(
+      (acceleration: DeviceMotionAccelerationData) => console.log(acceleration),
+      (error: any) => console.log(error)
+    );
+  }
+  
 }
